9f6047954aa25c4ad0d0d791fd398c755ab331ff
vlad <Vladislavgopienko10@gmail.com>
2024-11-25 16:34:08 +0600

    Добавил страницу для создания задач и её функционал

--- tasks/views.py
+++ tasks/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
-
-from django.shortcuts import render
+from django.shortcuts import redirect
+from .forms import TaskForm
 from .models import Task
 
 def task_list(request):
     tasks = Task.objects.all()
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+    return render(request, 'tasks/task_create.html', {'form': form})
